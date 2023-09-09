c8ed10b15e6e83b390cf029932f8974653bc43af
Joyce Vavilapalli <127877888+v-joyce@users.noreply.github.com>
2023-09-09 10:46:50 +0530

    Create encrypt2.py

---
+++ encrypt2.py
@@ -0,0 +1,11 @@
+def swap_letters(message):
+    letter_list = []
+    if not is_even(len(message)):
+        message = message + 'x'
+    even_letters = get_even_letters(message)
+    odd_letters = get_odd_letters(message)
+    for counter in range(0, int(len(message)/2)):
+        letter_list.append(odd_letters[counter])
+        letter_list.append(even_letters[counter])
+    new_message = ''.join(letter_list)
+    return new_message
